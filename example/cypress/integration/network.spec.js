9c48598de0b430ed7317a448ad1f68cfa457055c
Wenbo Jie <pinkyjie.gn@gmail.com>
2018-07-29 20:31:27 +1000

    add example folder

---
+++ example/cypress/integration/network.spec.js
@@ -0,0 +1,9 @@
+describe('Network test', function() {
+  it('should show the post data after "run" button is clicked', function() {
+    cy.visit('http://localhost:3000/');
+    cy.get('#xhr-btn').click();
+    cy.get('#xhr-result').should('contain', 'userId');
+    cy.get('#fetch-btn').click();
+    cy.get('#fetch-result').should('contain', 'id');
+  });
+});
