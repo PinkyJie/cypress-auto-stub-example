a5591bbdbf01f8840cbf1326acf3de08d9009026
Wenbo Jie <pinkyjie.gn@gmail.com>
2018-08-18 16:32:53 +1000

    typescript support

---
+++ cypress/global.d.ts
@@ -0,0 +1,4 @@
+declare namespace Cypress {
+  // tslint:disable-next-line interface-name
+  interface Chainable {}
+}
--- cypress/integration/network.spec.ts
+++ cypress/integration/network.spec.ts
@@ -1,6 +1,6 @@
 describe('Network test', function() {
   it('should show the post data after "run" button is clicked', function() {
-    cy.visit('http://localhost:3456/');
+    cy.visit('/');
     cy.get('#xhr-btn').click();
     cy.get('#xhr-result').should('contain', 'userId');
     cy.get('#fetch-btn').click();
