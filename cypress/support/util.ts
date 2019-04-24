38d62c6074787cd6ff2634e2caeaaff7dd7a7767
jhays-nhaschools <43037440+jhays-nhaschools@users.noreply.github.com>
2019-04-24 18:17:35 -0400

    Improve clarity of comments

--- cypress/support/util.ts
+++ cypress/support/util.ts
@@ -1,11 +1,11 @@
 /**
- * Block the test and wait for all API to be finished.
+ * Block the test and wait for all API calls to finished.
  *
  * We have a counter in cy.server()'s onRequest/onResponse callback, every time we send out
- * a API request, we increase the counter by 1, every time we receive a response, we decrease
- * the counter by 1. Inside this command, we block the test and this counter equals to 0.
+ * an API request, we increase the counter by 1, every time we receive a response, we decrease
+ * the counter by 1. Inside this command, we block the test until this counter equals 0.
  *
- * Call this command every time you expect a series of API requests which might takes longer.
+ * Call this command every time you expect a series of API requests which might take a while.
  */
 Cypress.Commands.add('waitUntilAllAPIFinished', () => {
   /**
@@ -13,8 +13,8 @@ Cypress.Commands.add('waitUntilAllAPIFinished', () => {
    * function continuously within the timeout you provided, until the expectation inside
    * that function has been met.
    *
-   * Note: the purpose of get('body') here is just to pass timeout to should() call,
-   * basically you can get any element on page.
+   * Note: the purpose of get('body') here is just to pass the timeout to the should() call,
+   * basically you can get any element on the page.
    */
   const timeout = Cypress.env('apiMaxWaitingTime') || 60 * 1000;
   cy.log('Waiting for pending API requests:');
